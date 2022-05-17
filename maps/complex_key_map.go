e319ad1a10547e81d76d4ee573d176da979c8098
Jed Liu <liujed@users.noreply.github.com>
2022-05-16 20:16:45 -0700

    Added ComplexKeyMap
    Useful for maps whose keys are complex enough that the map is
    represented in JSON as a list of key-value pairs.

---
+++ maps/complex_key_map.go
@@ -0,0 +1,101 @@
+package maps
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
+// A map whose keys are complex enough that the map is represented in JSON as
+// a list of key-value pairs.
+type ComplexKeyMap[K comparable, V any] map[K]V
+
+func (m ComplexKeyMap[K, V]) Put(k K, v V) {
+	m[k] = v
+}
+
+func (m ComplexKeyMap[K, V]) Upsert(k K, v V, onConflict func(v, newV V) V) {
+	newV := v
+	if oldV, exists := m[k]; exists {
+		newV = onConflict(oldV, newV)
+	}
+	m[k] = newV
+}
+
+func (m ComplexKeyMap[K, V]) Add(other ComplexKeyMap[K, V], onConflict func(v, newV V) V) {
+	for k, v := range other {
+		m.Upsert(k, v, onConflict)
+	}
+}
+
+func (m ComplexKeyMap[K, V]) Get(k K) V {
+	return m[k]
+}
+
+// Returns the value associated with the given key k. If the key does not
+// already exist in the map, the supplied function is called, and the resulting
+// value is entered into the map and returned.
+func (m ComplexKeyMap[K, V]) GetOrCompute(k K, computeValue func() (V, error)) (V, error) {
+	if v, exists := m[k]; exists {
+		return v, nil
+	}
+
+	v, err := computeValue()
+	if err != nil {
+		return v, err
+	}
+
+	m[k] = v
+	return v, nil
+}
+
+// Returns the value associated with the given key k. If the key does not
+// already exist in the map, the supplied default value is entered into the map
+// and returned.
+func (m ComplexKeyMap[K, V]) GetOrDefault(k K, defaultValue V) V {
+	v, exists := m[k]
+	if !exists {
+		v = defaultValue
+		m[k] = v
+	}
+	return v
+}
+
+func (m ComplexKeyMap[K, V]) ContainsKey(k K) bool {
+	_, exists := m[k]
+	return exists
+}
+
+func (m ComplexKeyMap[K, V]) IsEmpty() bool {
+	return len(m) == 0
+}
+
+type SliceElt[K comparable, V any] struct {
+	Key   K `json:"key"`
+	Value V `json:"value"`
+}
+
+func (m ComplexKeyMap[K, V]) MarshalJSON() ([]byte, error) {
+	slice := make([]SliceElt[K, V], 0, len(m))
+	for k, v := range m {
+		slice = append(slice, SliceElt[K, V]{
+			Key:   k,
+			Value: v,
+		})
+	}
+
+	return json.Marshal(slice)
+}
+
+func (m *ComplexKeyMap[K, V]) UnmarshalJSON(text []byte) error {
+	var slice []SliceElt[K, V]
+	if err := json.Unmarshal(text, &slice); err != nil {
+		return errors.Wrapf(err, "failed to unmarshal ComplexKeyMap")
+	}
+
+	*m = make(ComplexKeyMap[K, V], len(slice))
+	for _, elt := range slice {
+		(*m)[elt.Key] = elt.Value
+	}
+	return nil
+}
---
+++ maps/complex_key_map_test.go
@@ -0,0 +1,71 @@
+package maps
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/akitasoftware/go-utils/math"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicComplexKeyMapOperations(t *testing.T) {
+	m := ComplexKeyMap[[2]string, int]{}
+
+	assert.True(t, m.IsEmpty())
+
+	foo := [2]string{"foo", "bar"}
+	bar := [2]string{"bar", "baz"}
+	baz := [2]string{"baz", "qux"}
+	qux := [2]string{"qux", "quux"}
+
+	m.Upsert(foo, 1, math.Add[int])
+	assert.False(t, m.IsEmpty())
+	assert.Equal(t, ComplexKeyMap[[2]string, int]{foo: 1}, m)
+
+	m.Add(ComplexKeyMap[[2]string, int]{foo: 2, bar: 1}, math.Add[int])
+	assert.Equal(t, ComplexKeyMap[[2]string, int]{foo: 3, bar: 1}, m)
+
+	m.Put(foo, 42)
+	assert.Equal(t, ComplexKeyMap[[2]string, int]{foo: 42, bar: 1}, m)
+	assert.Equal(t, m.Get(foo), 42)
+	assert.Equal(t, m.GetOrDefault(foo, 19), 42)
+	assert.False(t, m.ContainsKey(baz))
+	assert.Equal(t, m.GetOrDefault(baz, 19), 19)
+	assert.True(t, m.ContainsKey(baz))
+	assert.Equal(t, m.Get(baz), 19)
+
+	result, err := m.GetOrCompute(foo, func() (int, error) { return 37, fmt.Errorf("error") })
+	assert.NoError(t, err)
+	assert.Equal(t, result, 42)
+
+	result, err = m.GetOrCompute(foo, func() (int, error) { return 37, nil })
+	assert.NoError(t, err)
+	assert.Equal(t, result, 42)
+
+	assert.False(t, m.ContainsKey(qux))
+
+	_, err = m.GetOrCompute(qux, func() (int, error) { return 37, fmt.Errorf("error") })
+	assert.Error(t, err)
+
+	result, err = m.GetOrCompute(qux, func() (int, error) { return 37, nil })
+	assert.NoError(t, err)
+	assert.Equal(t, result, 37)
+	assert.Equal(t, m.Get(qux), 37)
+}
+
+func TestComplexKeyMapJson(t *testing.T) {
+	m := ComplexKeyMap[[2]string, int]{}
+	m.Put([2]string{"foo", "bar"}, 3)
+	m.Put([2]string{"bar", "baz"}, 2)
+	m.Put([2]string{"baz", "qux"}, 1)
+
+	bytes, err := json.Marshal(m)
+	assert.NoError(t, err)
+
+	var deserialized ComplexKeyMap[[2]string, int]
+	err = json.Unmarshal(bytes, &deserialized)
+	assert.NoError(t, err)
+
+	assert.Equal(t, deserialized, m, "m == unmarshal(marshal(m))")
+}
