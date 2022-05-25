b2b6d1bacb27ff8ac8215b0772fd9ccb513fe376
Jed Liu <liujed@users.noreply.github.com>
2022-05-25 02:02:27 -0700

    Added FilterMap functions

--- slices/map.go
+++ slices/map.go
@@ -1,8 +1,10 @@
 package slices
 
+import "github.com/akitasoftware/go-utils/optionals"
+
 // Apply f to each element of slice in order, returning the results.
 func Map[T1, T2 any](slice []T1, f func(T1) T2) []T2 {
-	result, _ := MapWithErr[T1, T2](slice, func(t T1) (T2, error) {
+	result, _ := MapWithErr(slice, func(t T1) (T2, error) {
 		return f(t), nil
 	})
 	return result
@@ -11,17 +13,39 @@ func Map[T1, T2 any](slice []T1, f func(T1) T2) []T2 {
 // Apply f to each element of slice in order, returning the results.  Returns
 // an error if f returns a non-nil error on any element.
 func MapWithErr[T1, T2 any](slice []T1, f func(T1) (T2, error)) (rv []T2, err error) {
+	return FilterMapWithErr(slice, func(t1 T1) (optionals.Optional[T2], error) {
+		t2, err := f(t1)
+		return optionals.Some(t2), err
+	})
+}
+
+// Applies f to each element of slice in order, removes any None results, and
+// returns the rest.
+func FilterMap[T1, T2 any](slice []T1, f func(T1) optionals.Optional[T2]) []T2 {
+	result, _ := FilterMapWithErr(slice, func(t T1) (optionals.Optional[T2], error) {
+		return f(t), nil
+	})
+	return result
+}
+
+// Applies f to each element of slice in order, removes any None results, and
+// returns the rest. Returns an error if f returns a non-nil error on any
+// element.
+func FilterMapWithErr[T, U any](slice []T, f func(T) (optionals.Optional[U], error)) ([]U, error) {
 	if slice == nil {
 		return nil, nil
 	}
 
-	rv = make([]T2, len(slice))
-	for i, v := range slice {
-		rv[i], err = f(v)
+	result := make([]U, len(slice))
+	for _, t := range slice {
+		u_opt, err := f(t)
 		if err != nil {
 			return nil, err
 		}
+		if u, exists := u_opt.Get(); exists {
+			result = append(result, u)
+		}
 	}
 
-	return rv, nil
+	return result, nil
 }
