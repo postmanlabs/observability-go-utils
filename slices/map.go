6e85caefbf2ff699b1923eac1807e14cbdc894e1
Jed Liu <liujed@users.noreply.github.com>
2023-03-14 11:04:18 -0700

    Added filter functions for slices (#19)
    Also moved FilterMap and its variants to their own file, and
    standardized all implementations to call `FilterMapIndexWithErr`
    directly.

--- slices/map.go
+++ slices/map.go
@@ -4,16 +4,17 @@ import "github.com/akitasoftware/go-utils/optionals"
 
 // Apply f to each element of slice in order, returning the results.
 func Map[T1, T2 any](slice []T1, f func(T1) T2) []T2 {
-	result, _ := MapWithErr(slice, func(t T1) (T2, error) {
-		return f(t), nil
+	result, _ := FilterMapIndexWithErr(slice, func(_ int, t T1) (optionals.Optional[T2], error) {
+		return optionals.Some(f(t)), nil
 	})
 	return result
 }
 
-// Apply f to each element of slice in order, returning the results.  Returns
-// an error if f returns a non-nil error on any element.
+// Apply f to each element of slice in order, returning the results. If f
+// returns a non-nil error on any element, iteration immediately stops, and the
+// error is returned.
 func MapWithErr[T1, T2 any](slice []T1, f func(T1) (T2, error)) (rv []T2, err error) {
-	return FilterMapWithErr(slice, func(t1 T1) (optionals.Optional[T2], error) {
+	return FilterMapIndexWithErr(slice, func(_ int, t1 T1) (optionals.Optional[T2], error) {
 		t2, err := f(t1)
 		return optionals.Some(t2), err
 	})
@@ -21,8 +22,8 @@ func MapWithErr[T1, T2 any](slice []T1, f func(T1) (T2, error)) (rv []T2, err er
 
 // Like Map, but f also takes in the element's index.
 func MapIndex[T1, T2 any](slice []T1, f func(int, T1) T2) []T2 {
-	result, _ := MapIndexWithErr(slice, func(idx int, t T1) (T2, error) {
-		return f(idx, t), nil
+	result, _ := FilterMapIndexWithErr(slice, func(idx int, t T1) (optionals.Optional[T2], error) {
+		return optionals.Some(f(idx, t)), nil
 	})
 	return result
 }
@@ -33,50 +34,4 @@ func MapIndexWithErr[T1, T2 any](slice []T1, f func(int, T1) (T2, error)) (rv []
 		t2, err := f(idx, t1)
 		return optionals.Some(t2), err
 	})
-}
-
-// Applies f to each element of slice in order, removes any None results, and
-// returns the rest.
-func FilterMap[T1, T2 any](slice []T1, f func(T1) optionals.Optional[T2]) []T2 {
-	result, _ := FilterMapWithErr(slice, func(t T1) (optionals.Optional[T2], error) {
-		return f(t), nil
-	})
-	return result
-}
-
-// Applies f to each element of slice in order, removes any None results, and
-// returns the rest. Returns an error if f returns a non-nil error on any
-// element.
-func FilterMapWithErr[T, U any](slice []T, f func(T) (optionals.Optional[U], error)) ([]U, error) {
-	return FilterMapIndexWithErr(slice, func(_ int, t T) (optionals.Optional[U], error) {
-		return f(t)
-	})
-}
-
-// Like FilterMap, but f also takes in the element's index.
-func FilterMapIndex[T1, T2 any](slice []T1, f func(int, T1) optionals.Optional[T2]) []T2 {
-	result, _ := FilterMapIndexWithErr(slice, func(idx int, t T1) (optionals.Optional[T2], error) {
-		return f(idx, t), nil
-	})
-	return result
-}
-
-// Like FilterMapWithErr, but f also takes in the element's index.
-func FilterMapIndexWithErr[T, U any](slice []T, f func(int, T) (optionals.Optional[U], error)) ([]U, error) {
-	if slice == nil {
-		return nil, nil
-	}
-
-	result := make([]U, 0, len(slice))
-	for idx, t := range slice {
-		u_opt, err := f(idx, t)
-		if err != nil {
-			return nil, err
-		}
-		if u, exists := u_opt.Get(); exists {
-			result = append(result, u)
-		}
-	}
-
-	return result, nil
 }
